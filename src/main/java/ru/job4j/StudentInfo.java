530f5244a99cb0098e9a9b5b0d92c39896a2e6e3
vetrova.a <alyona.vetrova@list.ru>
2021-12-15 19:23:07 +0500

    6. Checkstyle [#4123 #231972]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Alena Vetrova");
+        System.out.println("29.04.1993");
+    }
+
+}
